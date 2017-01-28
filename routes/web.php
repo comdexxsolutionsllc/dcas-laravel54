085ddfc0f82fce04f1d01b3fa8321349823cd695
Sarah Renner <sarah@sarahrenner.work>
2017-01-28 03:19:26 -0600

    disallow direct registration by overriding route paths in web.php

--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,25 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Auth::routes();
+// Authentication Routes...
+Route::get('login', 'Auth\LoginController@showLoginForm')->name('login');
+Route::post('login', 'Auth\LoginController@login');
+Route::post('logout', 'Auth\LoginController@logout')->name('logout');
 
+// Registration Routes... // null
+Route::get('register', function() {
+    abort(403, 'Unauthorized action.');
+})->name('register');
+
+Route::post('register', function() {
+    abort(403, 'Unauthorized action.');
+});
+
+// Password Reset Routes...
+Route::get('password/reset', 'Auth\ForgotPasswordController@showLinkRequestForm')->name('password.request');
+Route::post('password/email', 'Auth\ForgotPasswordController@sendResetLinkEmail');
+Route::get('password/reset/{token}', 'Auth\ResetPasswordController@showResetForm')->name('password.reset');
+Route::post('password/reset', 'Auth\ResetPasswordController@reset');
+
+// Other pages...
 Route::get('/home', 'HomeController@index');
