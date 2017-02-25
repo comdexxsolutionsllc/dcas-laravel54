a20d556bf2f18704ff6f8bbf6af48e5528caed89
Sarah Renner <sarah@sarahrenner.work>
2017-02-25 16:50:40 -0600

    chat module, protect User object from unnecessary select(*)

--- Modules/Chat/Entities/ChatMessage.php
+++ Modules/Chat/Entities/ChatMessage.php
@@ -15,6 +15,6 @@ class ChatMessage extends Model {
      */
     public function user()
     {
-        return $this->belongsTo('\App\User');
+        return $this->belongsTo('\App\User')->select(array('id', 'name', 'email', 'is_admin'));;
     }
 }
