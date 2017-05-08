4e680bfda5a19223f699ba8ae4218607bcd5690b
Sarah Renner <sarah@sarahrenner.work>
2017-05-07 22:20:25 -0500

    update BaseModel

--- app/BaseModel.php
+++ app/BaseModel.php
@@ -2,6 +2,12 @@
 
 namespace App;
 
+use Carbon\Carbon;
+use Cmgmyr\Messenger\Traits\Messagable;
+use Illuminate\Notifications\Notifiable;
+use Session;
+use OwenIt\Auditing\Auditable;
+use OwenIt\Auditing\Contracts\Auditable as AuditableContract;
 use Cog\Ban\Contracts\HasBans as HasBansContract;
 use Cog\Ban\Traits\HasBans;
 use Nicolaslopezj\Searchable\SearchableTrait;
@@ -10,9 +16,9 @@ use Venturecraft\Revisionable\RevisionableTrait;
 use Watson\Rememberable\Rememberable;
 
 
-abstract class BaseModel extends Revisionable implements HasBansContract
+abstract class BaseModel extends Revisionable implements AuditableContract, HasBansContract
 {
-    use HasBans, Rememberable, RevisionableTrait, SearchableTrait;
+    use Auditable, HasBans, Messagable, Notifiable, Rememberable, RevisionableTrait, SearchableTrait;
 
     /**
      * Searchable rules.
