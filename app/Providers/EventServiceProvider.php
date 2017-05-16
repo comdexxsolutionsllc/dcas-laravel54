e939dbc8749c5ab958c2e1175ae32610046bbdb1
Sarah Renner <sarah@sarahrenner.work>
2017-05-16 16:44:58 -0500

    add Cache Listeners

--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -4,7 +4,8 @@ namespace App\Providers;
 
 use Illuminate\Foundation\Support\Providers\EventServiceProvider as ServiceProvider;
 
-class EventServiceProvider extends ServiceProvider {
+class EventServiceProvider extends ServiceProvider
+{
 
     /**
      * The event listener mappings for the application.
@@ -12,20 +13,35 @@ class EventServiceProvider extends ServiceProvider {
      * @var array
      */
     protected $listen = [
-        'Illuminate\Auth\Events\Lockout'           => [
+        'Illuminate\Auth\Events\Lockout' => [
             'App\Listeners\Users\UponLoginLockout',
         ],
-        'Illuminate\Auth\Events\Login'             => [
+        'Illuminate\Auth\Events\Login' => [
             'App\Listeners\Users\UpdateLastLoggedInAt',
         ],
-        'Illuminate\Auth\Events\Logout'            => [
+        'Illuminate\Auth\Events\Logout' => [
             'App\Listeners\Users\UponLogoutUpdateLastLoggedInAt',
         ],
         'Illuminate\Database\Events\QueryExecuted' => [
             'App\Listeners\Database\LogQueryExecution',
         ],
-        'Illuminate\Mail\Events\MessageSending'    => [
+        'Illuminate\Mail\Events\MessageSending' => [
             'App\Listeners\Mail\LogSentMessage',
+        ],
+        'Illuminate\Cache\Events\CacheHit' => [
+            'App\Listeners\LogCacheHit',
+        ],
+
+        'Illuminate\Cache\Events\CacheMissed' => [
+            'App\Listeners\LogCacheMissed',
+        ],
+
+        'Illuminate\Cache\Events\KeyForgotten' => [
+            'App\Listeners\LogKeyForgotten',
+        ],
+
+        'Illuminate\Cache\Events\KeyWritten' => [
+            'App\Listeners\LogKeyWritten',
         ],
     ];
 
